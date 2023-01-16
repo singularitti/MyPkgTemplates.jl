d3908905bd3e7691cf95bc882157fc40206e552b
singularitti <singularitti@outlook.com>
2023-01-15 20:54:55 -0500

    Fix docs/make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -16,12 +16,13 @@ makedocs(;
     pages=[
         "Home" => "index.md",
         "Manual" => [
-            "Installation guide" => "installation.md",
+            "Installation Guide" => "installation.md",
         ],
-        # "API Reference" => "public.md",
+        # "Public API" => "public.md",
         "Developer Docs" => [
             "Contributing" => "developers/contributing.md",
-            "Style Guide" => "developers/style.md",
+            "Style Guide" => "developers/style-guide.md",
+            "Design Principles" => "developers/design-principles.md",
         ],
         "Troubleshooting" => "troubleshooting.md",
     ]
